b234904c57d283ff01fa7d4e0172fe631c0f477a
jatinGoyal <jatingoyal100@gmail.com>
2020-08-07 23:59:40 +0530

    pepcoding

--- pepcoding/src/com/dsa/recursion/PrintNumbers.java
+++ pepcoding/src/com/dsa/recursion/PrintNumbers.java
@@ -49,3 +49,31 @@ public class PrintNumbers {
         printDecreasingIncreasing(5);
         }
 }
+/*
+Output :
+"C:\Program Files\Java\jdk1.8.0_202\bin\java.exe" "-javaagent:F:\NPTEL\MBA\Java IDE\Intellij 2019.3.4\lib\idea_rt.jar=50604:F:\NPTEL\MBA\Java IDE\Intellij 2019.3.4\bin" -Dfile.encoding=UTF-8 -classpath "C:\Program Files\Java\jdk1.8.0_202\jre\lib\charsets.jar;C:\Program Files\Java\jdk1.8.0_202\jre\lib\deploy.jar;C:\Program Files\Java\jdk1.8.0_202\jre\lib\ext\access-bridge-64.jar;C:\Program Files\Java\jdk1.8.0_202\jre\lib\ext\cldrdata.jar;C:\Program Files\Java\jdk1.8.0_202\jre\lib\ext\dnsns.jar;C:\Program Files\Java\jdk1.8.0_202\jre\lib\ext\jaccess.jar;C:\Program Files\Java\jdk1.8.0_202\jre\lib\ext\jfxrt.jar;C:\Program Files\Java\jdk1.8.0_202\jre\lib\ext\localedata.jar;C:\Program Files\Java\jdk1.8.0_202\jre\lib\ext\nashorn.jar;C:\Program Files\Java\jdk1.8.0_202\jre\lib\ext\sunec.jar;C:\Program Files\Java\jdk1.8.0_202\jre\lib\ext\sunjce_provider.jar;C:\Program Files\Java\jdk1.8.0_202\jre\lib\ext\sunmscapi.jar;C:\Program Files\Java\jdk1.8.0_202\jre\lib\ext\sunpkcs11.jar;C:\Program Files\Java\jdk1.8.0_202\jre\lib\ext\zipfs.jar;C:\Program Files\Java\jdk1.8.0_202\jre\lib\javaws.jar;C:\Program Files\Java\jdk1.8.0_202\jre\lib\jce.jar;C:\Program Files\Java\jdk1.8.0_202\jre\lib\jfr.jar;C:\Program Files\Java\jdk1.8.0_202\jre\lib\jfxswt.jar;C:\Program Files\Java\jdk1.8.0_202\jre\lib\jsse.jar;C:\Program Files\Java\jdk1.8.0_202\jre\lib\management-agent.jar;C:\Program Files\Java\jdk1.8.0_202\jre\lib\plugin.jar;C:\Program Files\Java\jdk1.8.0_202\jre\lib\resources.jar;C:\Program Files\Java\jdk1.8.0_202\jre\lib\rt.jar;E:\gitrepo\pepcoding\out\production\pepcoding" com.dsa.recursion.PrintNumbers
+5
+4
+3
+2
+1
+-----
+1
+2
+3
+4
+5
+-----
+5
+4
+3
+2
+1
+1
+2
+3
+4
+5
+
+
+*/
